a14493faef7477fc3ce771dda3dd8eea2c72c385
Jason Tyler <jason.tyler@knime.com>
2019-08-13 13:51:49 -0500

    Add project skeleton and boiler plate code.

---
+++ src/main/java/com/geekytex/githubissuesapi/GithubIssuesApiApplication.java
@@ -0,0 +1,13 @@
+package com.geekytex.githubissuesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIssuesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubIssuesApiApplication.class, args);
+    }
+
+}
